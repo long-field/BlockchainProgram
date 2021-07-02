edc45d56ea23e4e6e8be92cca26c7260a7683190
longfield <contact@dietervanlangenaker.be>
2021-07-02 14:23:55 +0200

    Initial commit

---
+++ src/main/java/be/Vanlangenaker/Domain/StringUtil.java
@@ -0,0 +1,26 @@
+package be.Vanlangenaker.Domain;
+
+import java.security.MessageDigest;
+
+public class StringUtil {
+    //Applie Sha256 to a string and return result.
+    public static String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
